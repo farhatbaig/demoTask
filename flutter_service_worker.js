455653dc16d73c5f6049709e2c0cbdc8a8f06fe2
farhatbaig <farhatbaig@users.noreply.github.com>
2024-12-08 16:58:19 +0000

    deploy: 5021013891074838932209da84a6f9522de8f9aa

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -43,8 +43,8 @@ const RESOURCES = {"icons/Icon-maskable-192.png": "c457ef57daa1d16f64b27b786ec2e
 "/": "9b92260cfa21f58dc46405f8648f1f00",
 "flutter.js": "f393d3c16b631f36852323de8e583132",
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
-"flutter_bootstrap.js": "c22d1b3a8a15900fc3ab8b8b7a4b76f2",
-"main.dart.js": "574bc52db483e9a780e9f79a94aed649"};
+"flutter_bootstrap.js": "33c8c31a73b85b78bb8574d9ab5b492e",
+"main.dart.js": "9140b39e469fcb68783c004488aa2fd1"};
 // The application shell files that are downloaded before a service worker can
 // start.
 const CORE = ["main.dart.js",
